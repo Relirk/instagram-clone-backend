3b835d4d28557546942918ba6a430434cc431d04
Francisco José Olvera Junior <olverajunior2014@gmail.com>
2019-06-18 01:10:52 -0300

    Alterando paths relativos

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 module.exports = {
     async index(req, res) {
-        const post = await Post.find().sort('-createdAt')
+        const post = await Post.find().sort('-createdAt');
         return res.json(post);
     },
 
@@ -13,14 +13,14 @@ module.exports = {
         const { author, place, description, hashtags } = req.body;
         const {filename: image } = req.file;
         const [name] = image.split('.');
-        const fileName = `${name}.jpg`;   
-    
+        const fileName = `${name}.jpg`;
+
         await sharp(req.file.path)
             .resize(500)
             .jpeg({quality: 70})
             .toFile(
                 path.resolve(req.file.destination, 'resized', fileName)
-            )
+            );
 
         fs.unlinkSync(req.file.path);
 
@@ -30,10 +30,10 @@ module.exports = {
             description,
             hashtags,
             image: fileName,
-        })
+        });
 
         req.io.emit('post', post);
-        
+
         return res.json(post);
     }
 };
